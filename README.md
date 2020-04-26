b6ab57ff1ef05de6b7f938051a9ca34361d5a2cb
Fabio Hora <fabio.hora@ipc-global.com>
2020-04-26 16:07:51 -0300

    Desafio concluido

---
+++ README.md
@@ -0,0 +1,23 @@
+
+<h1 align="center">
+  DESAFIO 03 RocketSeat GoStack BootCamp
+</h1>
+
+## :information_source: How To Use
+
+To clone and run this application, you'll need [Git](https://git-scm.com), [Node.js v12.13.1][nodejs] or higher + [Yarn v1.21.1][yarn] or higher installed on your computer. From your command line:
+
+```bash
+# Clone this repository
+$ git clone https://github.com/fabiohora/desafio03-gostack
+
+# Go into the repository
+$ cd desafio03-gostack
+
+# Install dependencies
+$ yarn
+
+# Run tests
+$ yarn test
+
+```
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import {
   SafeAreaView,
@@ -10,46 +10,75 @@ import {
   TouchableOpacity,
 } from "react-native";
 
+import api from './services/api';
+
 export default function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    const response = await api.post(`/repositories/${id}/like`);
+
+    const findRepoIndex = repositories.findIndex(repo =>
+      repo.id === id
+    );
+  
+    const newRepos = repositories.map(repository =>{
+      if (repository.id === response.data.id) {
+        return response.data
+      } else {
+        return repository
+      }
+    });
+
+    setRepositories(newRepos);
   }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
+      <FlatList
+          data={repositories}
+          keyExtractor={repository => repository.id}
+          renderItem={({ item: repository }) => (
         <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
+        <Text style={styles.repository}>{repository.title}</Text>
 
-          <View style={styles.techsContainer}>
+        {repository.techs.map(tech => (
+          <View style={styles.techsContainer} key={tech}>
             <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
+              {tech}
             </Text>
           </View>
+        ))}
 
           <View style={styles.likesContainer}>
             <Text
               style={styles.likeText}
               // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
+              testID={`repository-likes-${repository.id}`}
             >
-              3 curtidas
+              {repository.likes} {repository.likes > 1 ? 'curtidas' : 'curtida'}
             </Text>
           </View>
+          
 
           <TouchableOpacity
             style={styles.button}
-            onPress={() => handleLikeRepository(1)}
+            onPress={() => handleLikeRepository(repository.id)}
             // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
+            testID={`like-button-${repository.id}`}
           >
             <Text style={styles.buttonText}>Curtir</Text>
           </TouchableOpacity>
         </View>
+        )} />
       </SafeAreaView>
     </>
   );
